b30cedbe68d0f101676c9b35f45c52c85c0eb91a
Tushar Gupta <58504726+Tushargupta9800@users.noreply.github.com>
2020-10-14 12:44:40 +0530

    Update IIT2019043.cpp

--- Tasks/TASK 1/Submission/IIT2019043.cpp
+++ Tasks/TASK 1/Submission/IIT2019043.cpp
@@ -1,15 +1,15 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-const unsigned long long int mod = ((1<<63) - 1)*2 + 1;     //mod should be unsing long long int to handle such big value
+const unsigned long long int mod = ((1<<63) - 1)*2 - 57;     //mod should be unsing long long int to handle such big value
 const int p=26;             //p should be 26 as their are 26 letters in english alphabet
 //takes a string and returns a vector which containts the hash values
 //v[0] = hash(substr(0,1))
 //v[1] = hash(substr(0,2))
 //v[2] = hash(substr(0,3)) .... and so on
-vector <long long int> computeHash(char *str)
+vector <unsigned long long int> computeHash(char *str)
 {
-    vector <long long int> hash_values(strlen(str));
+    vector <unsigned long long int> hash_values(strlen(str));
 
     hash_values[0] = str[0] - 'a' + 1;      //we store hash_values[0] in prior
     long long int pp=26;                //then pp becomes 26
@@ -25,7 +25,7 @@ vector <long long int> computeHash(char *str)
 
 //this a simple function of binary power and only change in the 
 //function is to add unsined before long long int m
-long long int binpow(long long int a,long long int b,unsigned long long int m)
+unsigned long long int binpow(unsigned long long int a,unsigned long long int b,unsigned long long int m)
 {
 	long long int res=1;
 	while(b>0)
@@ -43,8 +43,8 @@ int main()
 {
 	char *str;cin>>str;
 	
-	vector <long long int> hash_prefix = computeHash(str);
-	vector <long long int> hash_suffix(strlen(str)-1);
+	vector <unsigned long long int> hash_prefix = computeHash(str);
+	vector <unsigned long long int> hash_suffix(strlen(str)-1);
 	
 	//fix some error in thisfor loop
 	for(long long int i=strlen(str);i>1;i--)
@@ -52,8 +52,8 @@ int main()
 		hash_suffix[strlen(str)-i] = ((hash_prefix[strlen(str) - 1] - hash_prefix[i-2])/binpow(p,i-1,mod))%mod;
 	}
 	
-	map <long long int,long long int> m_prefix;
-	map <long long int,long long int> m_suffix;
+	map <unsigned long long int,long long int> m_prefix;
+	map <unsigned long long int,long long int> m_suffix;
 	
 	for(int i=0;i<strlen(str)-1;i++)
 	{
