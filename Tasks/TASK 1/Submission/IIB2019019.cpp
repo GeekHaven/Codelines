ef532ef18e61e9d3e743ea1fcf01de6aeadb0c03
Kalpana200 <67147636+Kalpana200@users.noreply.github.com>
2020-10-14 23:55:02 +0530

    Update IIB2019019.cpp

--- Tasks/TASK 1/Submission/IIB2019019.cpp
+++ Tasks/TASK 1/Submission/IIB2019019.cpp
@@ -1,8 +1,8 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-const long long int mod = 1e9+9;
-const int p=31;
+const long long int mod = (1<<64)+53; //value of mod must be prime (1<<64)+53 is a prime and within constraint.
+const int p=31;      // closest prime number around number of alphabets.
 //takes a string and returns a vector which containts the hash values
 //v[0] = hash(substr(0,1))
 //v[1] = hash(substr(0,2))
@@ -11,9 +11,9 @@ vector <long long int> computeHash(string str)
 {
 	
 	vector <long long int> hash_values(str.length());
-    hash_values[0]=str[0]-'a'+1;
-	long long int pp=31;
-	for(int i=1;i<str.length();i++)
+    hash_values[0]=str[0]-'a'+1;  // proir setting value of hash_values[0] to calculate further values adding 1 to avoid dealing with zero in case of "a" ,"aa",....
+	long long int pp=31;    // initial value of pp =1 after first step value of pp=(1*31)%mod=31
+	for(int i=1;i<str.length();i++) //staring from i=1 to avoid runtime error as in this case i=0 , index of hash_values[i-1] will become -1 
 	{
 		hash_values[i] = (hash_values[i-1] + (str[i]-'a'+1)*pp)%mod;
 		pp = (pp * p)%mod;
@@ -21,7 +21,7 @@ vector <long long int> computeHash(string str)
 	return hash_values;
 	
 }
-long long binpow(unsigned long long a,unsigned long long b,unsigned long long m) {
+long long binpow(unsigned long long a,unsigned long long b,unsigned long long m) { 
     a %= m;
     long long res = 1;
     while (b > 0) {
@@ -38,12 +38,12 @@ int main()
 	string str;cin>>str;
 	
 	vector < long long int> hash_prefix = computeHash(str);
-    vector <unsigned long long int> hash_suffix(str.length()-1);
+    vector <unsigned long long int> hash_suffix(str.length()-1); // total number of suffix will be str.length()-1
 	
-	for(long long int i=str.length();i>1;i--)
+	for(long long int i=str.length();i>1;i--) // fix some errors to calculate hash_suffix
 	{
 		hash_suffix[str.length()-i] = ((hash_prefix[str.length()-1] - hash_prefix[i-2]) * binpow(binpow(p,i-1,mod),mod- 
-       2,mod))%mod;
+       2,mod))%mod;    // hash_prefix[str.length()-1] will contain whole string hash number , calculating hash_suffix vector by using already calculate hash_prefix vector.
 	}
 	map <long long int,long long int> m_prefix;
 	map <long long int,long long int> m_suffix;
@@ -51,16 +51,16 @@ int main()
 	for(int i=0;i<str.length()-1;i++)
 	{
 		m_prefix[hash_prefix[i]]++;
-		m_suffix[hash_suffix[i]]++;
+		m_suffix[hash_suffix[i]]++;  //hash_suffix value will be calculated in m_suffix map to that we could check later the presence of same substring
 	}
-	long long ans=0;
+	long long ans=0;  // changed datatype to long long to hold large size number.
 	
 	for(auto it:m_prefix)
 	{
 		long long int f1=it.second;
 		long long int f2=m_suffix[it.first];
 		
-		ans += f1 * f2;
+		ans += f1 * f2;   // adding + operator to calculate total number of prefixes which are also suffix.
 		
 	}
 	
